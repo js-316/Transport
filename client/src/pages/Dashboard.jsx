eb88e6d72012760dacadfe482c09f6981c39ee05
Jonathan Ssekyanzi <jonassekyanzi316@gmail.com>
2024-05-29 10:30:31 +0300

    Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -40,12 +40,7 @@ const Dashboard = () => {
           <h2 className="content-title card-title">My Dashboard </h2>
 
         </div>
-        <div>
-          <a href="#" className="btn btn-primary">
-            <i className="text-muted material-icons md-post_add">Create
-              report</i>
-          </a>
-        </div>
+        
       </div>
       <div className="row">
         {cardsData.map((cd, index) => (
@@ -102,122 +97,13 @@ const Dashboard = () => {
           </div>
 
           <div className="row">
-            <div className="col-lg-5">
-              <div className="card mb-4">
-                <article className="card-body">
-                  <h5 className="card-title">New Drivers</h5>
-                  <div className="new-member-list">
-                    {driversArray?.slice(-3).map((d, index) => (
-                      <div
-                        key={index}
-                        className="d-flex align-items-center justify-content-between mb-4"
-                      >
-                        <div className="d-flex align-items-center">
-                          <LetteredAvatar
-                            name={d.name}
-                            size={50}
-                            className="me-3"
-                          />
-                          <div>
-                            <h6>{d.name}</h6>
-                            <p className="text-muted font-xs">
-                              {new Date(d.date_hired).toLocaleDateString()}
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </article>
-              </div>
-            </div>
-            <div className="col-lg-7">
-              <div className="card mb-4">
-                <article className="card-body">
-                  <h5 className="card-title">Recent activities</h5>
-                  <ul className="verti-timeline list-unstyled font-sm">
-                    {[...Array(5)].map((_, index) => (
-                      <li key={index} className="event-list">
-                        <div className="event-timeline-dot">
-                          <i className="material-icons md-play_circle_outline font-xxl"></i>
-                        </div>
-                        <div className="media">
-                          <div className="me-3">
-                            <h6>
-                              <span>Today</span>{" "}
-                              <i className="material-icons md-trending_flat text-brand ml-15 d-inline-block"></i>
-                            </h6>
-                          </div>
-                          <div className="media-body">
-                            <div>More Data</div>
-                          </div>
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
-                </article>
-              </div>
-            </div>
+            
+            
           </div>
         </div>
 
       </div>
-      <div className="card mb-4">
-        <header className="card-header" data-select2-id="11">
-          <h4 className="card-title">Latest Fleet</h4>
-          <div className="row align-items-center" data-select2-id="10">
-            <div className="col-md-2 col-6">
-              <input type="date" value="02.05.2022" className="form-control" />
-            </div>
-          </div>
-        </header>
-        <div className="card-body">
-          <div className="table-responsive">
-            <div className="table-responsive">
-              <table className="table table-hover">
-                <thead>
-                  <tr>
-                    <th>Number Plate</th>
-                    <th>Drivers</th>
-                    <th>Mileage</th>
-                    <th>Manufacturer</th>
-                    <th>Date of Purchase</th>
-                    <th className="text-end"> Action </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {isLoading
-                    ? [...Array(5)].map((_, i) => (
-                      <TableLoader key={i} count={6} />
-                    ))
-                    : vehichlesArray?.map((v, index) => (
-                      <tr key={index}>
-                        <td>{v.number_plate}</td>
-                        <td>{v.driver.name}</td>
-                        <td>{v.mileage}</td>
-                        <td>{v.manufacturer}</td>
-                        <td>{v.date_of_purchase}</td>
-                        <td className="text-end">
-                          <Link
-                            to={`vehichles/edit/${v.id}`}
-                            className="btn btn-sm font-sm rounded btn-brand mx-4"
-                          >
-                            <i className="material-icons md-edit"></i>
-                            Edit
-                          </Link>
-                          <button className="btn btn-sm font-sm rounded btn-danger">
-                            <i className="material-icons md-delete"></i>
-                            Delete
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
+      
     </Layout>
   );
 };
