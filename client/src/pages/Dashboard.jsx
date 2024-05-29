08997830e7ef1acce73499c501b3f940ce66e4ae
HarrisonMoses <harrisonmoses64@gmail.com>
2024-05-29 12:42:24 +0300

    Some Ui Changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -10,40 +10,62 @@ import Charts from "../components/Charts";
 import LetteredAvatar from "react-lettered-avatar";
 import { useGetMaintenanceQuery } from "../features/maintenance/maintenanceApiSlice";
 import { useGetFuelQuery } from "../features/fuel/fuelApiSlice";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCar, faUser, faWrench, faTint } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCar,
+  faUser,
+  faWrench,
+  faTint,
+} from "@fortawesome/free-solid-svg-icons";
 import PriorityChart from "../components/PriorityChart";
 import FuelChart from "../components/FuelChart";
-import ServiceChart from "../components/ServiceChart"
-
+import ServiceChart from "../components/ServiceChart";
+import BarChart from "../components/Barchart";
+import Listgroups from "../components/Listgroups";
 
 const Dashboard = () => {
   const { isLoading, data } = useGetVehichlesQuery();
   const { data: drivers } = useGetDriversQuery();
-  const { data: maintenanceData } = useGetMaintenanceQuery()
-  const { data: fuelData } = useGetFuelQuery()
+  const { data: maintenanceData } = useGetMaintenanceQuery();
+  const { data: fuelData } = useGetFuelQuery();
 
   const { ids, entities } = data || {};
-  const { ids: driverIds, entities: driverEntities } = drivers || {}
-  const { ids: maintenanceIds, entities: maintenancesEntities } = maintenanceData || {}
-  const { ids: fuelIds, entities: fuelEntities } = fuelData || {}
+  const { ids: driverIds, entities: driverEntities } = drivers || {};
+  const { ids: maintenanceIds, entities: maintenancesEntities } =
+    maintenanceData || {};
+  const { ids: fuelIds, entities: fuelEntities } = fuelData || {};
 
   const vehichlesArray = ids?.map((id) => entities[id]);
-  const driversArray = driverIds?.map((id) => driverEntities[id])
-  const maintenancesArray = maintenanceIds?.map((id) => maintenancesEntities[id])
-  const fuelArray = maintenanceIds?.map((id) => fuelEntities[id])
+  const driversArray = driverIds?.map((id) => driverEntities[id]);
+  const maintenancesArray = maintenanceIds?.map(
+    (id) => maintenancesEntities[id]
+  );
+  const fuelArray = maintenanceIds?.map((id) => fuelEntities[id]);
+
+   const items = [
+     { text: "Service Reminders", badge: 14 },
+     { text: "Contact Renewals", badge: 2 },
+     { text: "Vehicle Renewals", badge: 1 },
+   ];
+   const issues = [
+     { text: "vehicle Failures", badge: 14 },
+     { text: "Faults", badge: 2 },
+   ];
+   const workorder = [
+     { text: "Work Orders", badge: 14 },
+   ];
 
   return (
     <Layout>
       <div className="content-header">
         <div>
           <h2 className="content-title card-title">My Dashboard </h2>
-
         </div>
         <div>
           <a href="#" className="btn btn-primary">
-            <i className="text-muted material-icons md-post_add">Create
-              report</i>
+            <i className="text-muted material-icons md-post_add">
+              Create report
+            </i>
           </a>
         </div>
       </div>
@@ -62,8 +84,7 @@ const Dashboard = () => {
         ))}
       </div>
       <div className="row">
-        <div className="col-md-6">
-
+        <div className="col-md-10">
           <div className="row">
             <div className="col-md-6">
               <div className="card mb-4">
@@ -95,12 +116,11 @@ const Dashboard = () => {
               <div className="card mb-4">
                 <article className="card-body">
                   <h5 className="card-title">Service Costs</h5>
-                  <ServiceChart />
+                  <BarChart />
                 </article>
               </div>
             </div>
           </div>
-
           <div className="row">
             <div className="col-lg-5">
               <div className="card mb-4">
@@ -160,7 +180,17 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
-
+        <div className="col-md-2">
+          <div className="card mb-3">
+            <Listgroups title="Remiders" items={items} />
+          </div>
+          <div className="card mb-3">
+            <Listgroups title="Issues" items={issues} />
+          </div>
+          <div className="card mb-3">
+            <Listgroups title="Maintanace" items={workorder} />
+          </div>
+        </div>
       </div>
       <div className="card mb-4">
         <header className="card-header" data-select2-id="11">
@@ -188,30 +218,30 @@ const Dashboard = () => {
                 <tbody>
                   {isLoading
                     ? [...Array(5)].map((_, i) => (
-                      <TableLoader key={i} count={6} />
-                    ))
+                        <TableLoader key={i} count={6} />
+                      ))
                     : vehichlesArray?.map((v, index) => (
-                      <tr key={index}>
-                        <td>{v.number_plate}</td>
-                        <td>{v.driver.name}</td>
-                        <td>{v.mileage}</td>
-                        <td>{v.manufacturer}</td>
-                        <td>{v.date_of_purchase}</td>
-                        <td className="text-end">
-                          <Link
-                            to={`/vehichles/${v.id}`}
-                            className="btn btn-sm font-sm rounded btn-brand mx-4"
-                          >
-                            <i className="material-icons md-edit"></i>
-                            Edit
-                          </Link>
-                          <button className="btn btn-sm font-sm rounded btn-danger">
-                            <i className="material-icons md-delete"></i>
-                            Delete
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
+                        <tr key={index}>
+                          <td>{v.number_plate}</td>
+                          <td>{v.driver.name}</td>
+                          <td>{v.mileage}</td>
+                          <td>{v.manufacturer}</td>
+                          <td>{v.date_of_purchase}</td>
+                          <td className="text-end">
+                            <Link
+                              to={`/vehichles/${v.id}`}
+                              className="btn btn-sm font-sm rounded btn-brand mx-4"
+                            >
+                              <i className="material-icons md-edit"></i>
+                              Edit
+                            </Link>
+                            <button className="btn btn-sm font-sm rounded btn-danger">
+                              <i className="material-icons md-delete"></i>
+                              Delete
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
                 </tbody>
               </table>
             </div>
