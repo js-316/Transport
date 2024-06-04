a136a0db086795d41681acecf1feaf26d54d74aa
HarrisonMoses <harrisonmoses64@gmail.com>
2024-06-04 11:35:31 +0300

    Some few Changes

--- client/src/pages/Maintenance.jsx
+++ client/src/pages/Maintenance.jsx
@@ -163,7 +163,7 @@ const Maintenance = () => {
     doc.text(`Total Cost: ${totalCost}`, 10, 20);
 
     // Prepare table data
-    const tableData = vehicleMaintenances.map((record) => [
+    const tableData = vehicleMaintenances?.map((record) => [
         record.date,
         record.description,
         record.cost,
@@ -231,7 +231,7 @@ const Maintenance = () => {
               className="form-select"
               onChange={(e) => costsExportToPDF(e.target.value)}>
                 <option>Calculate Cost</option>
-                {uniqueVehicles.map((number_plate, index) => (
+                {uniqueVehicles?.map((number_plate, index) => (
                 <option key={index} value={number_plate}>{number_plate}</option>
                 ))}
               </select>
