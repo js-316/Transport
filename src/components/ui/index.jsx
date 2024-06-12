33b8ec1bbb14791743c37e594d30f3e48bac7c14
Jonathan Ssekyanzi <77006421+js-316@users.noreply.github.com>
2024-06-12 10:57:27 +0300

    Delete src directory

--- src/components/ui/index.jsx
+++
@@ -1,9 +0,0 @@
-import PasswordInput from "./inputs/PasswordInput";
-import EmailInput from "./inputs/EmailInput";
-import TextInput from "./inputs/TextInput";
-import NumberInput from "./inputs/NumberInput";
-import TextAreaInput from "./inputs/TextAreaInput";
-import DateInput from "./inputs/DateInput";
-
-
-export {PasswordInput, EmailInput, TextInput, NumberInput, TextAreaInput, DateInput};
